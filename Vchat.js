57d02d5f345cb457c9b92c14863eaed447e45a8f
Shubham <shubhamgangurde16@gmail.com>
2022-05-10 09:31:52 +0530

    final commit of Vchat

--- Vchat.js
+++ Vchat.js
@@ -1,46 +1,186 @@
-let client= AgoraRTC.createClient({mode:'rtc','codec':"vp8"})
+//making the client object
+let client = AgoraRTC.createClient({mode:'rtc', codec:"vp8"})
 
-//configutration of agora client
-let config= {
-    appid:'c35e626e1bee45119633477c9c85756f',
-    tocken:'006c35e626e1bee45119633477c9c85756fIACGP9lAd7lyFDPB1OPcStmDpuekhhPipxY4tZnzIIi/J1Qub9IAAAAAEAA/6Ep2TbF3YgEAAQBNsXdi',
+//configuring the authentication of agora
+let config = {
+    appid:'0cb393c13c3042a2b9161b87fb7f8657',
+    token:'0060cb393c13c3042a2b9161b87fb7f8657IACmj28+9QOdickLI4oAzkkVvJ5DH3BnDl4ZrVsSHhXjwG4CxRQAAAAAEAD9FOMnhyF7YgEAAQCHIXti+kyEy9rH3uXpPoZpsAAAAAEAD9FOMnqiB7YgEAAQCqIHti',
     uid:null,
-    channel:'videochat',
+    channel:'pict',
 }
 
-
-//our own video and audio tracks
-let localTracks= {
-    audioTracks:null,
-    videoTracks:null,
+// Setting tracks for when user joins
+let localTracks = {
+    audioTrack:null,
+    videoTrack:null
 }
 
-
-//video and audio tracks of other user thats gonna be join to our application
-let remoteTracks = {
-
+// Want to hold state for users audio and video so user can mute and hide
+let localTrackState = {
+    audioTrackMuted:false,
+    videoTrackMuted:false
 }
 
+//Set remote tracks to store other users
+let remoteTracks = {}
 
-document.getElementById("join-btn").addEventListener('click', async()=>{
-    console.log("user wants to join steam");
-    await joinStreams();
+
+document.getElementById('join-btn').addEventListener('click', async () => {
+    config.uid = document.getElementById('username').value
+    await joinStreams()
+    document.getElementById('join-wrapper').style.display = 'none'
+    document.getElementById('footer').style.display = 'flex'
 })
 
-let joinStreams = async()=>{
-    [config.uid, localTracks.audioTracks,localTracks.videoTracks]= await Promise.all([
-        client.join(config.appid, config.channel, config.token || n),
+document.getElementById('mic-btn').addEventListener('click', async () => {
+    //Check if what the state of muted currently is
+    //Disable button
+    if(!localTrackState.audioTrackMuted){
+        //Mute your audio
+        await localTracks.audioTrack.setMuted(true);
+        localTrackState.audioTrackMuted = true
+        document.getElementById('mic-btn').style.backgroundColor ='rgb(255, 80, 80, 0.7)'
+    }else{
+        await localTracks.audioTrack.setMuted(false)
+        localTrackState.audioTrackMuted = false
+        document.getElementById('mic-btn').style.backgroundColor ='#1f1f1f8e'
+
+    }
+
+})
+
+
+
+document.getElementById('camera-btn').addEventListener('click', async () => {
+    //Check if what the state of muted currently is
+    //Disable button
+    if(!localTrackState.videoTrackMuted){
+        //Mute your audio
+        await localTracks.videoTrack.setMuted(true);
+        localTrackState.videoTrackMuted = true
+        document.getElementById('camera-btn').style.backgroundColor ='rgb(255, 80, 80, 0.7)'
+    }else{
+        await localTracks.videoTrack.setMuted(false)
+        localTrackState.videoTrackMuted = false
+        document.getElementById('camera-btn').style.backgroundColor ='#1f1f1f8e'
+
+    }
+
+})
+
+
+
+document.getElementById('leave-btn').addEventListener('click', async () => {
+    //Loop threw local tracks and stop them so unpublish event gets triggered, then set to undefined
+    //Hide footer
+    for (trackName in localTracks){
+        let track = localTracks[trackName]
+        if(track){
+            track.stop()
+            track.close()
+            localTracks[trackName] = null
+        }
+    }
+
+    //Leave the channel
+    await client.leave()
+    document.getElementById('footer').style.display = 'none'
+    document.getElementById('user-streams').innerHTML = ''
+    document.getElementById('join-wrapper').style.display = 'block'
+
+})
+
+
+
+
+//Method will take all my info and set user stream in frame  of website
+let joinStreams = async () => {
+    
+    client.on("user-published", handleUserJoined);
+    client.on("user-left", handleUserLeft);
+
+
+    client.enableAudioVolumeIndicator(); // Triggers the "volume-indicator" callback event every two seconds.
+    client.on("volume-indicator", function(evt){
+        for (let i = 0; evt.length > i; i++){
+            let speaker = evt[i].uid
+            let volume = evt[i].level
+            if(volume > 0){
+                document.getElementById(`volume-${speaker}`).src = './assets/volume-on.svg'
+            }else{
+                document.getElementById(`volume-${speaker}`).src = './assets/volume-off.svg'
+            }
+            
+        
+            
+        }
+    });
+
+    // Set and get back tracks for local user
+    [config.uid, localTracks.audioTrack, localTracks.videoTrack] = await  Promise.all([
+        client.join(config.appid, config.channel, config.token ||null, config.uid ||null),
         AgoraRTC.createMicrophoneAudioTrack(),
-        AgoraRTC.createCameraVideoTrack(),
+        AgoraRTC.createCameraVideoTrack()
+
     ])
-    let videoPlayer= `<div class="video-containers" id="video-wrapper-${config.uid}"> 
-                    <p class= "user-id">${config.uid}</p>
-                    <div class="video-player player" id="stream-${config.uid}"></div>
-                     </div>`
+    
+    //Create player and add it to player list
+    let player = `<div class="video-containers" id="video-wrapper-${config.uid}">
+                        <p class="user-uid"><img class="volume-icon" id="volume-${config.uid}" src="./assets/volume-on.svg" /> ${config.uid}</p>
+                        <div class="video-player player" id="stream-${config.uid}"></div>
+                  </div>`
 
-document.getElementById('user-streams').insertAdjacentHTML('beforeend',videoPlayer)
-localTracks.videoTracks.play(`stream-${config.uid}`)
+    document.getElementById('user-streams').insertAdjacentHTML('beforeend', player);
+    localTracks.videoTrack.play(`stream-${config.uid}`)
+    
 
-await client.publish([localTracks.audioTracks, localTracks.videoTracks])
+    //Add user to user list of names/ids
 
+    // Publish my local video tracks to entire channel so everyone can see it
+    await client.publish([localTracks.audioTrack, localTracks.videoTrack])
+
+}
+
+
+let handleUserJoined = async (user, mediaType) => {
+    console.log('Handle user joined')
+
+    // Add user to list of remote users
+    remoteTracks[user.uid] = user
+
+    //Subscribe ro remote users
+    await client.subscribe(user, mediaType)
+   
+    
+    if (mediaType === 'video'){
+        let player = document.getElementById(`video-wrapper-${user.uid}`)
+        console.log('player:', player)
+        if (player != null){
+            player.remove()
+        }
+ 
+        player = `<div class="video-containers" id="video-wrapper-${user.uid}">
+                        <p class="user-uid"><img class="volume-icon" id="volume-${user.uid}" src="./assets/volume-on.svg" /> ${user.uid}</p>
+                        <div  class="video-player player" id="stream-${user.uid}"></div>
+                      </div>`
+        document.getElementById('user-streams').insertAdjacentHTML('beforeend', player);
+         user.videoTrack.play(`stream-${user.uid}`)
+
+        
+
+          
+    }
+    
+
+    if (mediaType === 'audio') {
+        user.audioTrack.play();
+      }
+}
+
+
+let handleUserLeft = (user) => {
+    console.log('Handle user left!')
+    //Remove from remote users and remove users video wrapper
+    delete remoteTracks[user.uid]
+    document.getElementById(`video-wrapper-${user.uid}`).remove()
 }
